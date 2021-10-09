a120ca48aa02f0167b9061d6dc9a3502a8d07034
KasymovD <ksmv982@gmail.com>
2021-10-09 19:44:28 +0600

    Kaimak

--- main.py
+++ main.py
@@ -1,28 +1,97 @@
+from kivy.core.window import Window
 from kivy.properties import StringProperty, ObjectProperty
 from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.core.window import Window
-from kivymd.uix.pickers import MDTimePicker
+from kivymd.uix.button import MDRoundFlatButton, MDRectangleFlatIconButton
+from kivymd.uix.card import MDCard, MDSeparator
+from kivymd.uix.picker import MDTimePicker
+from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
+from kivymd.uix.snackbar import Snackbar
+from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
+from kivy.properties import StringProperty, ListProperty
+
+from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.list import OneLineIconListItem, MDList
 
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.floatlayout import MDFloatLayout
-
 from kivymd.uix.taptargetview import MDTapTargetView
 
-Builder.load_file('main.kv')
+KV = open('main.kv', 'r').read()
+
+
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
 
 class Tab(MDFloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
     content_text = StringProperty("")
 
-class MyLayout(Widget):
-    pass
 
-class AwesomeApp(MDApp):
+class MyApp(MDApp):
+    def dob(self):
+        # box = MDRectangleFlatIconButton(
+        #         icon= "account-multiple",
+        #         text= 'Шоро',
+        #         theme_text_color= "Custom",
+        #         text_color= (0,0,0,1),
+        #         line_color= (0,0,0,0),
+        #         icon_color= (0,0,0,1),
+        #         pos_hint={"center_x": 0.4, "center_y": .88},
+        #         size_hint=(2,0.08),
+        #     )
+        # self.root.ids.tatar.add_widget(box)
+        pass
+
     def build(self):
         Window.size = (360, 640)
-        return MyLayout()
+        return Builder.load_string(KV)
+
+
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+
+    # def on_start(self):
+        # self.root.ids.tabs.add_widget(Tab(text="One"))
+        # self.root.ids.tabs.add_widget(Tab(text="Two"))
+        # self.root.ids.tabs.add_widget(Tab(text="One"))
+        # self.root.ids.tabs.add_widget(Tab(text="Two"))
+
+
 
     def show_time_picker(self):
         '''Open time picker dialog.'''
@@ -44,4 +113,4 @@ class AwesomeApp(MDApp):
 
 if __name__=="__main__":
 
-    AwesomeApp().run()
+    MyApp().run()
